80c8f817e861f4a461eaad94304e6e163bdc3aeb
btran946 <105252771+btran946@users.noreply.github.com>
2022-11-08 14:51:06 -0800

    Bare bones reactflow

--- app/Main/page.tsx
+++ app/Main/page.tsx
@@ -1,4 +1,12 @@
+import Flow from "./(flow)/Flow";
+
 export default function Page() {
-    return <h1>Welcome to MainPage</h1>;
-  }
-  
+  return (
+    // Parent component of reactflow needs a height and width in order to display
+    <div style={{ height: "500px", width: "500px" }}>
+      <h1>Welcome to MainPage</h1>
+      <h1>flow</h1>
+      <Flow />
+    </div>
+  );
+}
